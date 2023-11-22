1ba2d8c8d9976971d0f7b3d73347ec9ae4d472df
Israel Ogunmola <israelogunmola@gmail.com>
2023-11-23 00:56:59 +0100

    feat: Refactor play game function

--- lib/game.rb
+++ lib/game.rb
@@ -5,51 +5,60 @@ require './lib/player'
 # Handles all game attributes and functions
 class Game
   def initialize
-    @words = load_words
-    @selected_word = select_word(@words)
+    @selected_word = select_word
     @letters_guessed = []
     @board = '_' * @selected_word.length
     @player = Player.new
     display_intro
     # puts @board
     # puts @selected_word
+    puts @selected_word
+  end
+
+  # working here
+  def play_game
+    guesses_left = 10
+    guesses_left.times do |i|
+      if playround
+    end
   end
 
   def play_round
-    10.downto(1) do |i|
-      display_status(i)
-      letter = validate_player_choice(@player.choose_letter)
-      update_board(letter)
-      return puts @board if board_full?
+    choice = validate_player_choice(@player.choose_letter)
+    if @selected_word.include?(choice)
+      puts 'Nice Guess!'
+      update_board(choice)
+    else
+      puts 'Not Quite'
     end
-    puts "You lost! You couldn't guess the word: #{@selected_word}"
   end
 
   def display_intro
     puts <<~HEREDOC
+
       H-A-N-G-M-A-N
       Would you like to: [1] Start a new game
                          [2] Load a game
-      Enter '1' or '2'"
+      Enter '1' or '2'
+
     HEREDOC
   end
 
   def display_status(counter)
-    puts "Letters guessed: #{@letters_guessed.join(' ')}"
-    puts "Incorrect guesses left: #{counter}"
-    puts @board
-    puts
+    puts <<~HEREDOC
+
+      Letters guessed: #{@letters_guessed.join(' ')}
+      Incorrect guesses left: #{counter}
+      #{@board}
+
+    HEREDOC
   end
 
   def update_board(letter)
-    if @selected_word.include?(letter)
-      puts 'Good guess!'
-      @selected_word.length.times do |i|
-        @board[i] = letter if @selected_word[i] == letter
-      end
-    else
-      puts 'Not quite!'
+    @selected_word.length.times do |i|
+      @board[i] = letter if @selected_word[i] == letter
     end
+    true
   end
 
   def validate_player_choice(letter)
@@ -72,18 +81,16 @@ class Game
     @letters_guessed.include?(letter)
   end
 
-  def board_full?
+  def winner?
     @selected_word == @board
   end
 
-  def select_word(words)
-    words.select { |word| word.length.between?(5, 12) }.sample
-  end
-
-  def load_words
+  def select_word
     words = File.open('google-10000-english-no-swears.txt', 'r', &:readlines)
-    words.map(&:chomp)
+    words = words.map(&:chomp)
+    words.select { |word| word.length.between?(5, 12) }.sample
   end
 end
 
-Game.new.play_round
+test = Game.new
+10.times { |_i| play_round }
